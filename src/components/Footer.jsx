baa4b6080ed2425841fef5100c60aa5480395c5d
LUIS OTAVIO DE OLIVEIRA SANTOS <105260695+LuisOtavioOliveira@users.noreply.github.com>
2023-01-14 17:16:46 -0300

    ChangeFooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-        <h2> Criado por Otávio </h2>
+        <h2> Criado por Luís Otávio </h2>
     </div>
   )
 }
